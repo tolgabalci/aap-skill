622b08bb33278b17a967fad14606a19ab5d44c37
tolgabalci <tolga.balci@gmail.com>
2020-08-27 21:31:24 +0000

    Now using Log4j

--- src/main/java/com/advancestores/hackathon/alexa/controller/Speedperks.java
+++ src/main/java/com/advancestores/hackathon/alexa/controller/Speedperks.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController
 public class Speedperks {
 
@@ -16,7 +19,8 @@ public class Speedperks {
 
     @GetMapping("/speedperks/{id}")
     public ResponseEntity<CouponDetails> getSpeedPerkDetails() {
-        System.out.println("getSpeedPerkDetails called");
+        // System.out.println("getSpeedPerkDetails called");
+        log.info("getSpeedPerkDetails called");
 
         CouponDetails couponDetails = speedPerkService.getCoupons();
         return new ResponseEntity<CouponDetails>(couponDetails, HttpStatus.OK);
