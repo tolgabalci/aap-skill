8d4b2e5bd3706bd53f716f705e6a4fabd89a9cd2
Veerendra <Veerendra.Tiwari@advance-auto.com>
2020-08-27 20:49:00 -0400

    added member search by phone

--- src/main/java/com/advancestores/hackathon/alexa/controller/Speedperks.java
+++ src/main/java/com/advancestores/hackathon/alexa/controller/Speedperks.java
@@ -8,19 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.advancestores.hackathon.alexa.service.SpeedPerkService;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
+@RequestMapping("/speedperks")
+@Log4j2
 public class Speedperks {
 
     @Autowired
     SpeedPerkService speedPerkService;
 
-    @GetMapping("/speedperks/{id}")
-    public ResponseEntity<String> getSpeedPerkDetails(@PathVariable String id) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-    	ResponseEntity<String> response= speedPerkService.getCoupons(id);
-        return response;
+    @GetMapping("/coupons/{accountNumber}")
+    public ResponseEntity<String> getSpeedPerkDetails(@PathVariable String accountNumber) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    	log.info("Get Coupons for accountNumber ->"+accountNumber);
+        return speedPerkService.getCoupons(accountNumber);
+    }
+    
+    @GetMapping("/members/{phone}")
+    public ResponseEntity<String> getMembersByPhone(@PathVariable String phone){
+    	log.info("Get members by phone number ->" + phone);
+    	return speedPerkService.getMembersByPhone(phone);
     }
 }
--- src/main/java/com/advancestores/hackathon/alexa/service/SpeedPerkService.java
+++ src/main/java/com/advancestores/hackathon/alexa/service/SpeedPerkService.java
@@ -5,6 +5,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -23,19 +24,17 @@ public class SpeedPerkService {
 
     @Autowired
     private RestTemplate restTemplate;
+    
+    @Autowired
+    private Environment env;
 
-    public ResponseEntity<String> getCoupons(String id) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://apiqa.test.advanceauto.cloud/api/v2/experience/loyalty/coupons")
-                .queryParam("accountNumber", id);
+    public ResponseEntity<String> getCoupons(String accountNumber) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(env.getProperty("speedperks.baseurl")+"/coupons")
+                .queryParam("accountNumber", accountNumber);
        
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("x-correlation-id", "some-uuid");
-        headers.set("client_id", "6f36fbeadad84b18b09b0cf7f974f741");
-        headers.set("client_secret", "Ea37a2ED33BC4eFAb3f349A9993e6FaB");
-        headers.set("Host", "experience-client-loyalty-api-v2.test.advanceauto.cloud");
-        HttpEntity entity = new HttpEntity(headers);
-        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
-        log.info("response ->" +response);
+        HttpEntity entity = getHttpEntity();
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, getHttpEntity(), String.class);
+        log.info("Coupons Response by accountNumer ->" +response);
         return response;
     }
     
@@ -44,4 +43,21 @@ public class SpeedPerkService {
     }
 
 
+    private HttpEntity getHttpEntity() {
+    	HttpHeaders headers = new HttpHeaders();
+        headers.set("x-correlation-id", "some-uuid");
+        headers.set("client_id", "6f36fbeadad84b18b09b0cf7f974f741");
+        headers.set("client_secret", "Ea37a2ED33BC4eFAb3f349A9993e6FaB");
+        headers.set("Host", "experience-client-loyalty-api-v2.test.advanceauto.cloud");
+        return new HttpEntity(headers);
+    }
+
+	public ResponseEntity<String> getMembersByPhone(String phone) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(env.getProperty("speedperks.baseurl")+"/members")
+                .queryParam("phone", phone);
+       
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, getHttpEntity(), String.class);
+        log.info("members Response by phone ->" + response);
+        return response;
+	}
 }
